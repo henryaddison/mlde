defadf753bc01bd02652b30b18253563c0095bd2
Henry Addison <henryaddison@users.noreply.github.com>
2024-06-25 13:31:38 +0100

    update default dataset names in configs

--- src/ml_downscaling_emulator/score_sde_pytorch/configs/subvpsde/ukcp_local_mv_12em_cncsnpp_continuous.py
+++ src/ml_downscaling_emulator/score_sde_pytorch/configs/subvpsde/ukcp_local_mv_12em_cncsnpp_continuous.py
@@ -35,7 +35,7 @@ def get_config():
   # data
   data = config.data
   data.centered = True
-  data.dataset_name = 'bham_ccpm-4x_12em_mv_random-season'
+  data.dataset_name = 'bham64_ccpm-4x_12em_mv'
 
   # model
   model = config.model
