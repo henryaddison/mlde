1879f5d0da80fbc7d4dfb210a2897fa50cd6f73d
Henry Addison <henryaddison@users.noreply.github.com>
2025-07-14 12:24:10 +0100

    a go at adapting a bcsd implementation

--- src/ml_downscaling_emulator/bin/__init__.py
+++ src/ml_downscaling_emulator/bin/__init__.py
@@ -1,11 +1,11 @@
 import typer
 
-from . import postprocess, sample
+from . import bcsd, postprocess, sample
 
 app = typer.Typer()
+app.add_typer(bcsd.app, name="bcsd")
 app.add_typer(sample.app, name="sample")
 app.add_typer(postprocess.app, name="postprocess")
-
 
 if __name__ == "__main__":
     app()
---
+++ src/ml_downscaling_emulator/bin/bcsd.py
@@ -0,0 +1,302 @@
+# Copyright 2025 The swirl_dynamics Authors.
+# Modifications copyright 2025 Henry Addison
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Significant modifications to the original work have been made by Henry Addison
+# to work with their UK Met Office CPM emulator framework.
+# Based on https://github.com/google-research/swirl-dynamics/blob/b79f03f78a64fb9259412ee62c591400484ecbff/swirl_dynamics/projects/probabilistic_diffusion/downscaling/gcm_wrf/analysis/bcsd.py and https://github.com/google-research/swirl-dynamics/blob/b79f03f78a64fb9259412ee62c591400484ecbff/swirl_dynamics/projects/probabilistic_diffusion/downscaling/gcm_wrf/input_pipelines/filter_in_space.py
+
+import functools
+import logging
+import os
+from pathlib import Path
+from scipy import ndimage
+import shortuuid
+import typer
+import xarray as xr
+
+from mlde_utils import samples_path, DEFAULT_ENSEMBLE_MEMBER
+from mlde_utils.training.dataset import open_raw_dataset_split
+from ml_downscaling_emulator.bin.climatology import compute_daily_stat
+from ml_downscaling_emulator.bin.sample import _np_samples_to_xr
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s - %(filename)s - %(asctime)s - %(message)s",
+)
+logger = logging.getLogger()
+logger.setLevel("INFO")
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    pass
+
+
+@app.command()
+def bcsd(
+    workdir: Path,
+    dataset: str,
+    train_dataset: str,
+    variable: str = "pr",
+    split: str = "test",
+    ensemble_member: str = DEFAULT_ENSEMBLE_MEMBER,
+):
+    r"""Script to perform downscaling with the time-aligned BCSD method.
+
+    This script implements the Bias Correction and Spatial Disaggregation (BCSD)
+    method for downscaling (Wood et al, 2002): https://doi.org/10.1029/2001JD000659.
+    This implementation follows the time-aligned BCSD method of Thrasher et al (2012):
+    https://doi.org/10.5194/hess-16-3309-2012. The methodology is modified to
+    operate on a single domain discretization (the high-resolution grid).
+
+    The method assumes we have a low-resolution dataset, its climatology, and the
+    spatially filtered and unfiltered climatology of a high-resolution dataset. The
+    input low-resolution dataset and all climatologies are assumed to have been
+    interpolated to the high-resolution grid for convenience. Nevertheless, the
+    low-resolution and filtered high-resolution fields are assumed to have a low
+    effective --and similar-- spatial resolution, since interpolation does not yield
+    energy at the interpolated lengthscales.
+
+    The bias correction step is performed by computing the percentiles of the input
+    dataset with respect to its climatology, and mapping them to the full field
+    values corresponding to the same percentiles in the filtered high-resolution
+    climatology.
+
+    The spatial disaggregation consists, for most variables, of subtracting the
+    climatological mean of the filtered high-resolution field from the
+    bias-corrected low-resolution field, and then adding the climatological mean of
+    the unfiltered high-resolution field. For some variables, such as precipitation,
+    we use a multiplicative correction instead: we multiply the bias-corrected
+    low-resolution field by the ratio of the filtered and unfiltered climatological
+    means. The variables to be corrected multiplicatively are specified by the
+    `multiplicative_vars` flag.
+
+    Although the temporal disaggregation step of Wood et al. (2002) is omitted in
+    our implementation (Thrasher et al., 2012), we describe it here for completeness.
+    Let the low-resolution data correspond to time averages over periods `T`. In the
+    temporal disaggregation step, for each sample resulting from the spatial
+    disaggregation step, we sample a high-resolution data sequence over period `T`
+    and the same time of the year as the spatially
+    disaggregated sample. The high--resolution sample sequence is drawn at random
+    from the dataset used to construct the high-resolution climatology. Once the
+    sample sequence is drawn, we scale it such that the time average over `T`
+    matches the spatially disaggregated sample. Finally, we use this scaled sequence
+    as the final BCSD sample.
+
+    Example usage:
+
+    ```
+    # Path to input dataset climatology.
+    INPUT_DATA_CLIM=<input_data_clim>
+
+    # Path to spatially filtered target climatology.
+    FILTERED_TARGET_CLIM=<filtered_target_clim>
+
+    # Path to unfiltered target climatology.
+    TARGET_CLIM=<target_clim>
+
+    # Path to dataset to be downscaled.
+    INPUT_DATA=<parent_dir>/low_resolution_dataset.zarr
+
+    # Time range to be downscaled.
+    TIME_START=2094
+    TIME_STOP=2097
+
+    # Path to the output dataset.
+    OUTPUT_PATH=<parent_dir>/bcsd_downscaled_dataset_${TIME_START}_${TIME_STOP}.zarr
+
+    mlde bcsd bcsd \
+    --input_data=${INPUT_DATA} \
+    --input_data_clim=${INPUT_DATA_CLIM} \
+    --filtered_target_clim=${FILTERED_TARGET_CLIM} \
+    --target_clim=${TARGET_CLIM} \
+    --time_start=${TIME_START} \
+    --time_stop=${TIME_STOP} \
+    --output_path=${OUTPUT_PATH} \
+    --multiplicative_vars=RAIN_24h
+    ```
+
+    """
+
+    output_dirpath = samples_path(
+        workdir=workdir,
+        checkpoint=f"epoch-0",
+        dataset=dataset,
+        input_xfm=f"{train_dataset}-bcsd",
+        split=split,
+        ensemble_member=ensemble_member,
+    )
+    os.makedirs(output_dirpath, exist_ok=True)
+
+    train_ds = open_raw_dataset_split(train_dataset, "train")
+
+    eval_ds = open_raw_dataset_split(dataset, split).sel(
+        ensemble_member=[ensemble_member]
+    )
+
+    logger.info(f"Loading low-res and target {variable}.")
+    lr_da = train_ds[f"lin{variable}"]
+    target_da = train_ds[f"target_{variable}"]
+
+    logger.info(f"Low-pass filtering target {variable}.")
+    filtered_da = _low_pass_filter(
+        target_da.to_dataset(),
+        scale=(60.0 / 8.8) / 2.0,
+        spatial_dims=("grid_longitude", "grid_latitude"),
+    )[f"target_{variable}"]
+
+    typer.echo("Running BCSD...")
+
+    bcsd_da = _bcsd_on_chunks(
+        source=eval_ds[f"lin{variable}"],
+        lr_da=lr_da,
+        filtered_da=filtered_da,
+        target_da=target_da,
+    )
+
+    output_filepath = os.path.join(output_dirpath, f"predictions-{shortuuid.uuid()}.nc")
+
+    cf_data_vars = {
+        key: eval_ds.data_vars[key]
+        for key in [
+            "rotated_latitude_longitude",
+            "time_bnds",
+            "grid_latitude_bnds",
+            "grid_longitude_bnds",
+        ]
+        if key in eval_ds.variables
+    }
+    coords = eval_ds.coords
+
+    xr_samples = _np_samples_to_xr(
+        bcsd_da.values, coords=coords, target_transform=None, cf_data_vars=cf_data_vars
+    )
+
+    logger.info(f"Saving BCSD predictions to {output_filepath}")
+    xr_samples[f"pred_{variable}"].encoding.update(zlib=True, complevel=5)
+    xr_samples.to_netcdf(output_filepath)
+
+
+def _bcsd_on_chunks(
+    source: xr.Dataset,
+    lr_da: xr.Dataset,
+    filtered_da: xr.Dataset,
+    target_da: xr.Dataset,
+    method: str = "gaussian",
+    multiplicative: bool = True,
+) -> xr.Dataset:
+    """Process an input data chunk with the BCSD method.
+
+    Args:
+      source: The source data chunk to be processed with the BCSD method.
+      original_clim: The climatology of the source data.
+      filtered_clim: The climatology of the spatially filtered target data.
+      target_clim: The climatology of the unfiltered target data.
+      method: The method to use for quantile mapping.
+      multiplicative: Whether to use multiplicative correction (e.g. for precipitation variables).
+    Returns:
+      The BCSD-downscaled data as an xarray Dataset.
+    """
+    sel = dict(
+        dayofyear=source["time"].dt.dayofyear,
+        #   hour=source['time'].dt.hour,
+        drop=True,
+    )
+    window_size = 3
+    # variables = [str(key) for key in source.keys()]
+    if method == "gaussian":
+        # clim_mean = _get_climatology_mean(original_clim, variables, **sel)
+        # clim_std = _get_climatology_std(original_clim, variables, **sel)
+        clim_mean = compute_daily_stat(
+            lr_da, window_size=window_size, stat_fn="mean"
+        ).sel(**sel)
+        clim_std = compute_daily_stat(
+            lr_da, window_size=window_size, stat_fn="std"
+        ).sel(**sel)
+
+        # Standardize with respect to the original climatology.
+        source_standard = (source - clim_mean) / clim_std
+
+        # Get value of the same quantile in the filtered climatology, keep anom.
+        # filtered_clim_std = _get_climatology_std(filtered_clim, variables, **sel)
+        filtered_clim_std = compute_daily_stat(
+            filtered_da, window_size=window_size, stat_fn="std"
+        ).sel(**sel)
+        source_bc_anom = source_standard * filtered_clim_std
+
+        # Add anom to the mean of the unfiltered climatology.
+        # target_clim_mean = _get_climatology_mean(target_clim, variables, **sel)
+        target_clim_mean = compute_daily_stat(
+            target_da, window_size=window_size, stat_fn="mean"
+        ).sel(**sel)
+
+        # source_bcsd = target_clim_mean + source_bc_anom
+
+        # Use multiplicative correction for precipitation variables
+        if multiplicative:  # MULTIPLICATIVE_VARS.value:
+            # filtered_clim_mean = _get_climatology_mean(
+            #     filtered_clim, MULTIPLICATIVE_VARS.value, **sel
+            # )
+            filtered_clim_mean = compute_daily_stat(
+                filtered_da, window_size=window_size, stat_fn="mean"
+            ).sel(**sel)
+            # for var in MULTIPLICATIVE_VARS.value:
+            # Compute the full field corresponding to the filtered climatology
+            # quantile. Then, keep the multiplicative anomaly.
+            bc_mult_anom = (source_bc_anom + filtered_clim_mean) / (filtered_clim_mean)
+            # Multiply the anomaly by the target climatology.
+            var_bcsd = bc_mult_anom * target_clim_mean
+            # source_bcsd = source_bcsd.drop_vars(var).assign(**{var: var_bcsd})
+
+        return var_bcsd.drop_vars(
+            ["dayofyear"]
+        )  # source_bcsd.drop_vars(['hour', 'dayofyear'])
+    else:
+        raise ValueError(f"BCSD method {method} not yet implemented.")
+
+
+def _low_pass_filter(
+    source: xr.Dataset,
+    *,
+    scale: float = (60.0 / 8.8)
+    / 2.0,  # default is half the scale factor from 60km input to 8.8km target
+    mode: str = "nearest",
+    spatial_dims: tuple[str, str] = ("grid_latitude", "grid_longitude"),
+) -> xr.Dataset:
+    """Filters a chunk in its spatial dimensions.
+
+    Args:
+        source: The source dataset chunk to interpolate to a new spatial grid.
+        scale: Scale (in pixels) used to construct the Gaussian filter.
+        mode: Determines how the input array is extended when the filter overlaps a
+        border.
+        spatial_dims: The dimensions of the source dataset to filter over.
+
+    Returns:
+        The spatially filtered chunk.
+    """
+    # Ordered dimensions of the source dataset can be fetched from a data_var.
+    # Dataset dimensions are not guaranteed to be ordered.
+    ordered_dims = source[list(source.data_vars)[0]].dims
+    # We only filter the spatial dimensions.
+    scales = tuple(scale * float(dim in spatial_dims) for dim in ordered_dims)
+
+    gaussian_filter = functools.partial(
+        ndimage.gaussian_filter,
+        sigma=scales,
+    )
+    return xr.apply_ufunc(gaussian_filter, source.load())
