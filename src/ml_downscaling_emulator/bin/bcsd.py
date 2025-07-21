e2e6e0034607a4bd21fc2dad5c720268fba86781
Henry Addison <henryaddison@users.noreply.github.com>
2025-07-21 13:07:44 +0100

    move bcsd code to lib

--- src/ml_downscaling_emulator/bin/bcsd.py
+++ src/ml_downscaling_emulator/bin/bcsd.py
@@ -17,20 +17,17 @@
 # to work with their UK Met Office CPM emulator framework.
 # Based on https://github.com/google-research/swirl-dynamics/blob/b79f03f78a64fb9259412ee62c591400484ecbff/swirl_dynamics/projects/probabilistic_diffusion/downscaling/gcm_wrf/analysis/bcsd.py and https://github.com/google-research/swirl-dynamics/blob/b79f03f78a64fb9259412ee62c591400484ecbff/swirl_dynamics/projects/probabilistic_diffusion/downscaling/gcm_wrf/input_pipelines/filter_in_space.py
 
-import functools
 import logging
-import numpy as np
 import os
 from pathlib import Path
-import scipy
 import shortuuid
 import typer
 import xarray as xr
 
 from mlde_utils import samples_path, DEFAULT_ENSEMBLE_MEMBER, TIME_PERIODS
 from mlde_utils.training.dataset import open_raw_dataset_split
-from ml_downscaling_emulator.bin.climatology import compute_daily_stat
 from ml_downscaling_emulator.bin.sample import _np_samples_to_xr
+from ml_downscaling_emulator.statistical_downscaling import bcsd_on_chunks, bcsd2
 
 logging.basicConfig(
     level=logging.INFO,
@@ -171,9 +168,9 @@ def bcsd(
         typer.echo(f"Running BCSD for {tp}...")
 
         if version == "v1":
-            bcsd_func = _bcsd_on_chunks
+            bcsd_func = bcsd_on_chunks
         elif version == "v2":
-            bcsd_func = _bcsd2
+            bcsd_func = bcsd2
         else:
             raise ValueError(
                 f"Unknown BCSD version: {version}. Supported versions: v1, v2."
@@ -213,192 +210,3 @@ def bcsd(
     logger.info(f"Saving BCSD predictions to {output_filepath}")
     xr_samples[f"pred_{variable}"].encoding.update(zlib=True, complevel=5)
     xr_samples.to_netcdf(output_filepath)
-
-
-def _bcsd_on_chunks(
-    source: xr.DataArray,
-    lr_da: xr.DataArray,
-    target_da: xr.DataArray,
-    method: str = "gaussian",
-    multiplicative: bool = True,
-    window_size: int = 3,
-) -> xr.DataArray:
-    """Process an input data chunk with the BCSD method.
-
-    Args:
-      source: The source data to be processed with the BCSD method.
-      lr_da: The low-resolution data used to fit the BCSD method.
-
-      target_da: The unfiltered target data for fitting the BCSD method.
-      method: The method to use for quantile mapping.
-      multiplicative: Whether to use multiplicative correction (e.g. for precipitation variables).
-      window_size: The size of the window used to compute the climatology statistics.
-    Returns:
-      The BCSD-downscaled data as an xarray DataArray.
-    """
-
-    logger.info(f"Low-pass filtering target data.")
-    # Compute the spatially filtered target data for fitting the BCSD method.
-    filtered_da = _low_pass_filter(
-        target_da,
-        scale=(60.0 / 8.8) / 2.0,
-        spatial_dims=("grid_longitude", "grid_latitude"),
-    )
-
-    sel = dict(
-        dayofyear=source["time"].dt.dayofyear,
-        drop=True,
-    )
-
-    if method == "gaussian":
-        # Compute the climatology of the low-resolution data.
-        logger.info(f"Computing low-res climatology")
-        clim_mean = compute_daily_stat(
-            lr_da, window_size=window_size, stat_fn="mean"
-        ).sel(**sel)
-        clim_std = compute_daily_stat(
-            lr_da, window_size=window_size, stat_fn="std"
-        ).sel(**sel)
-
-        # Standardize with respect to the original climatology.
-        source_standard = (source - clim_mean) / clim_std
-
-        # Get value of the same quantile in the filtered climatology, keep anom.
-        logger.info(f"Computing filtered hi-res climatology")
-        filtered_clim_std = compute_daily_stat(
-            filtered_da, window_size=window_size, stat_fn="std"
-        ).sel(**sel)
-        source_bc_anom = source_standard * filtered_clim_std
-
-        # Add anom to the mean of the unfiltered climatology.
-        logger.info(f"Computing hi-res climatology")
-        target_clim_mean = compute_daily_stat(
-            target_da, window_size=window_size, stat_fn="mean"
-        ).sel(**sel)
-
-        source_bcsd = target_clim_mean + source_bc_anom
-
-        # Use multiplicative correction for precipitation variables
-        if multiplicative:
-            filtered_clim_mean = compute_daily_stat(
-                filtered_da, window_size=window_size, stat_fn="mean"
-            ).sel(**sel)
-            # Compute the full field corresponding to the filtered climatology
-            # quantile. Then, keep the multiplicative anomaly.
-            bc_mult_anom = (source_bc_anom + filtered_clim_mean) / (filtered_clim_mean)
-            # Multiply the anomaly by the target climatology.
-            var_bcsd = bc_mult_anom * target_clim_mean
-            source_bcsd = var_bcsd
-
-        # Ensure no negative precip values in the output.
-        source_bcsd = source_bcsd.clip(min=0)
-
-        return source_bcsd.drop_vars(["dayofyear"])
-    else:
-        raise ValueError(f"BCSD method {method} not yet implemented.")
-
-
-def _low_pass_filter(
-    source: xr.DataArray,
-    *,
-    scale: float = (60.0 / 8.8)
-    / 2.0,  # default is half the scale factor from 60km input to 8.8km target
-    mode: str = "nearest",
-    spatial_dims: tuple[str, str] = ("grid_latitude", "grid_longitude"),
-) -> xr.Dataset:
-    """Filters a chunk in its spatial dimensions.
-
-    Args:
-        source: The source dataset chunk to interpolate to a new spatial grid.
-        scale: Scale (in pixels) used to construct the Gaussian filter.
-        mode: Determines how the input array is extended when the filter overlaps a
-        border.
-        spatial_dims: The dimensions of the source dataset to filter over.
-
-    Returns:
-        The spatially filtered chunk.
-    """
-    # Ordered dimensions of the source dataset can be fetched from a data_var.
-    # Dataset dimensions are not guaranteed to be ordered.
-    ordered_dims = source.dims
-    # We only filter the spatial dimensions.
-    scales = tuple(scale * float(dim in spatial_dims) for dim in ordered_dims)
-
-    gaussian_filter = functools.partial(
-        scipy.ndimage.gaussian_filter,
-        sigma=scales,
-    )
-    return xr.apply_ufunc(gaussian_filter, source.load())
-
-
-def _bcsd2(
-    source: xr.DataArray,
-    lr_da: xr.DataArray,
-    target_da: xr.DataArray,
-    method: str = "gaussian",
-    multiplicative: bool = True,
-    window_size: int = 3,
-) -> xr.DataArray:
-    """Process an input data chunk with the BCSD method.
-
-    Args:
-      source: The source data to be processed with the BCSD method.
-      lr_da: The low-resolution data used to fit the BCSD method.
-
-      target_da: The unfiltered target data for fitting the BCSD method.
-      method: The method to use for quantile mapping.
-      multiplicative: Whether to use multiplicative correction (e.g. for precipitation variables).
-      window_size: The size of the window used to compute the climatology statistics.
-    Returns:
-      The BCSD-downscaled data as an xarray DataArray.
-    """
-    # compute the quantile corresponding to the threshold in the high-res data for each location
-    target_threshold = 0.1 / (
-        24 * 60 * 60
-    )  # 0.1 mm/day wet threshold for precipitation
-
-    # thresh_q = (hr_pr <= threshold).mean(dim=["time", "ensemble_member"]) # simpler but incorrect? guess at quantiles for each location
-    thresh_q = (
-        xr.apply_ufunc(
-            scipy.stats.percentileofscore,
-            target_da.stack(ex=["time", "ensemble_member"]),
-            target_threshold,
-            input_core_dims=[["ex"], []],
-            vectorize=True,
-        )
-        / 100
-    )
-
-    # convert quantiles to the corresponding threshold in the low-res data
-    bcthresh = xr.apply_ufunc(
-        np.quantile,
-        lr_da,
-        thresh_q,
-        input_core_dims=[["time", "ensemble_member"], []],
-        vectorize=True,
-    )
-
-    # exclude dry days based on bias corrected threshold for the low-res data
-    # to ensure same proportion of days are dry for each location in high-res and low-res data
-    source = source.where(source >= bcthresh)
-    lr_da = lr_da.where(lr_da >= bcthresh)
-    target_da = target_da.where(target_da >= target_threshold)
-
-    # square root transform the wet-day data to make it more Gaussian-like
-    source = np.pow(source, 1 / 2)
-    lr_da = np.pow(lr_da, 1 / 2)
-    target_da = np.pow(target_da, 1 / 2)
-
-    bcsd_da = _bcsd_on_chunks(
-        source=source,
-        lr_da=lr_da,
-        target_da=target_da,
-        window_size=window_size,
-    )
-    # square the data to reverse the square root transform.
-    bcsd_da = np.pow(bcsd_da, 2)
-
-    # re-add any dry days as zeros
-    bcsd_da.fillna(0)
-
-    return bcsd_da
