fc4e91f1bbf4840b9f67f6a1ab03c9b04831723f
Henry Addison <henryaddison@users.noreply.github.com>
2025-12-01 12:22:09 +0000

    add config for RCMGEM on ALPS in hist-fut emulator mode

---
+++ src/ml_downscaling_emulator/configs/subvpsde/cordex_ml_mv_hist_fut_alps_cncsnpp_continuous.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+# Modifications copyright 2025 Henry Addison.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Training NCSN++ on CORDEX-ML-BENCH multivariate data with sub-VP SDE for historical and future emulator experiment."""
+from ml_downscaling_emulator.configs.subvpsde.cordex_ml_mv_cncsnpp_continuous_defaults import get_config as get_default_configs
+
+
+def get_config():
+  config = get_default_configs()
+
+  # data
+  data = config.data
+  data.dataset_name = 'ALPS_domain-Emulator_hist_future-CNRMCM5-perfect'
+
+  return config
