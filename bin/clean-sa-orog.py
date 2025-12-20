0de30719b6cbcec4d5356a0fbe65dce5e26238f0
Henry Addison <henryaddison@users.noreply.github.com>
2025-12-20 19:46:13 +0000

    clean up orog for SA ESD mode

--- bin/clean-sa-orog.py
+++ bin/clean-sa-orog.py
@@ -5,8 +5,17 @@ import dotenv
 dotenv.load_dotenv()
 
 
-satarg = xr.open_dataset(os.path.join(os.getenv("DATA_PATH"), "datasets/SA_domain-Emulator_hist_future-ACCESSCM2-perfect/train/target/pr_tasmax.nc"))
+for mode in ["Emulator_hist_future", "ESD_pseudo_reality"]:
+    satarg = xr.open_dataset(
+        os.path.join(
+            os.getenv("DATA_PATH"),
+            f"datasets/SA_domain-{mode}-ACCESSCM2-perfect/train/target/pr_tasmax.nc",
+        )
+    )
 
-stat_filepath=os.path.join(os.getenv("DATA_PATH"), "datasets/SA_domain-Emulator_hist_future-ACCESSCM2-perfect/train/predictors/Static_fields.nc")
-# xr.load_dataset(stat_filepath).drop_dims("time").sel(lat=satarg.lat, lon=satarg.lon).to_netcdf(stat_filepath)
-xr.load_dataset(stat_filepath).drop_dims("bnds").to_netcdf(stat_filepath)
+    stat_filepath = os.path.join(
+        os.getenv("DATA_PATH"),
+        f"datasets/SA_domain-{mode}-ACCESSCM2-perfect/train/predictors/Static_fields.nc",
+    )
+    # xr.load_dataset(stat_filepath).drop_dims("time").sel(lat=satarg.lat, lon=satarg.lon).to_netcdf(stat_filepath)
+    xr.load_dataset(stat_filepath).drop_dims("bnds").to_netcdf(stat_filepath)
