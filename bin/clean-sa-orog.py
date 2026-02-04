60c9bed46d894e3f9082a8a8a1faaef38cfae959
Henry Addison <henryaddison@users.noreply.github.com>
2026-02-04 11:56:44 +0000

    drop time dim and select correct lat and lon for SA orog

--- bin/clean-sa-orog.py
+++ bin/clean-sa-orog.py
@@ -17,5 +17,7 @@ for mode in ["Emulator_hist_future", "ESD_pseudo_reality"]:
         os.getenv("DATA_PATH"),
         f"datasets/SA_domain-{mode}-ACCESSCM2-perfect/train/predictors/Static_fields.nc",
     )
-    # xr.load_dataset(stat_filepath).drop_dims("time").sel(lat=satarg.lat, lon=satarg.lon).to_netcdf(stat_filepath)
+    xr.load_dataset(stat_filepath).drop_dims("time").sel(
+        lat=satarg.lat, lon=satarg.lon
+    ).to_netcdf(stat_filepath)
     xr.load_dataset(stat_filepath).drop_dims("bnds").to_netcdf(stat_filepath)
